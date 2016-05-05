4957601df819d46b7d46b3c89b8ed9258def6116
vagrant <vagrant@docker>
2016-05-05 18:41:56 +0000

    update

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -6,6 +6,7 @@ public class JavaHelloWorld
      {   
         System.out.println ("Hello World");
         System.out.println ("Hello Again");
+        System.out.println ("Hello Again");
 
      }
 }
